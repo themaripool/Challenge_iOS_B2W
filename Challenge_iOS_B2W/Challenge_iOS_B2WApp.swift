6c853e5d026dad61ac2a63a4d2b16de1bf37f438
themaripool <marielama@outlook.com.br>
2021-07-13 22:10:13 -0300

    Initial Commit

---
+++ Challenge_iOS_B2W/Challenge_iOS_B2WApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_iOS_B2WApp.swift
+//  Challenge_iOS_B2W
+//
+//  Created by Mariela Mendonça de Andrade on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_iOS_B2WApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge_iOS_B2W/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge_iOS_B2W
+//
+//  Created by Mariela Mendonça de Andrade on 13/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
